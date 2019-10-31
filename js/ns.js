8814e680fe402ebe07fe4c776e52ed3a2915d948
elaizae <43956037+elaizae@users.noreply.github.com>
2019-10-31 23:03:56 +0100

    laatste versie

--- js/ns.js
+++ js/ns.js
@@ -14,24 +14,18 @@ knop.addEventListener("click", function () {
 });
 
 
+var show = this.classList.toggle("search");
 
+var content = this.nextElementSibling;
+if (content.style.display === "block") {
+    content.style.display = "none";
+} else {
+    content.style.display = "block";
+}
 
-
-
-/* https://www.w3schools.com/howto/howto_js_collapsible.asp */
-
-var coll = document.getElementsByClassName("searchbutton");
+var bar = document.getElementsByClassName("searchbutton");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("search");
-
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
+for (i = 0; i < bar.length; i++) {
+    bar[i].addEventListener("click", show());
 }
